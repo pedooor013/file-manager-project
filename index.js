820c2bc76e08afaf13bc49c5d8b4c5adfd3b7bf0
Pedro Lopes <pedrolopes.programador013@gmail.com>
2025-08-22 16:15:23 -0300

    fixx: resolução do problema de ao criar arquivo não retornar nenhuma mensagem e não inserir conteúdo no arquivo criado

--- index.js
+++ index.js
@@ -1,65 +1,66 @@
-const fileManager = require('./fileManager');
-const readLineSync = require('readline-sync');
-const path = require('path');
+    const fileManager = require("./fileManager");
+    const readLineSync = require("readline-sync");
+    const path = require("path");
 
-function main(){
-    while(true){
+    async function main() {
+    const baseDir = path.join(__dirname, "my_files");
+    fileManager.createDirectory(baseDir);
 
+    while (true) {
         console.log(`
-            \nMenu:
-            \n1. Criar arquivo
-            \n2. Listar arquivos
-            \n3. Ler arquivo
-            \n4. Escrever arquivo
-            \n5. Deletar arquivo
-            \n6. Sair
-        `);
+                \nMenu:
+                \n1. Criar arquivo
+                \n2. Listar arquivos
+                \n3. Ler arquivo
+                \n4. Escrever arquivo
+                \n5. Deletar arquivo
+                \n6. Sair
+            `);
 
         const choice = readLineSync.question("Escolha uma opção:");
 
-        const baseDir = path.join(__dirname,"my_files");
+        switch (choice) {
+        case "1":
+            const fileName = readLineSync.question("Digite o nome do arquivo: ");
+            const fileContent = readLineSync.question(
+            "Digite o conteudo do novo arquivo (Ou deixe em branco): "
+            );
 
-        switch(choice){
-            case '1':
+            //dirname é o nome do diretorio atual
+            const createFilePath = path.join(baseDir, fileName);
 
-                const fileName = readLineSync.question('Digite o nome do arquivo: ');
-                const fileContent = readLineSync.question('Digite o conteudo do novo arquivo (Ou deixe em branco): ');
+            const fileMessage = await fileManager.createFile(
+            createFilePath,
+            fileContent
+            );
 
-                //dirname é o nome do diretorio atual
-                const createFilePath = path.join(__dirname, fileName);
+            console.log(fileMessage);
+            break;
 
-                const fileMessage = fileManager.createFile(createFilePath, fileContent);
+        case "2":
+            console.log("Arquivos do diretorio: ");
+            break;
 
-                console.log(fileMessage);
-                break;
+        case "3":
+            console.log("Conteúdo do arquivo:");
+            break;
 
-            case '2':
+        case "4":
+            console.log("Arquivo escrito com sucesso!");
+            break;
 
-            
-                console.log('Arquivos do diretorio: ');
-                break;
+        case "5":
+            console.log("Arquivo deletado com sucesso!");
+            break;
 
-            case '3':
-                console.log('Conteúdo do arquivo:');
-                break;
-
-            case '4':
-                console.log('Arquivo escrito com sucesso!');
-                break;
-
-            case '5':
-                console.log('Arquivo deletado com sucesso!');
-                break;
-
-            case '6':
-                console.log('Saindo...');
-                return;
-                break;
-            default:
-                console.log('Opção inválida. Tente novamente!')
-
-            }  
+        case "6":
+            console.log("Saindo...");
+            return;
+            break;
+        default:
+            console.log("Opção inválida. Tente novamente!");
+        }
     }
-}
+    }
 
-main();
+    main();
